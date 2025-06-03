924304006e649c56c6f615fd56c25a67dec719b1
Azeqqen <azamatk171@gmail.com>
2025-06-03 15:19:26 +0500

    Add files via upload

---
+++ Lab1 codes/variables/assign multiple values.py
@@ -0,0 +1,19 @@
+#ASSIGN MULTIPLE VALUES
+#
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+#
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+#
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
---
+++ Lab1 codes/variables/python variables.py
@@ -0,0 +1,39 @@
+#PYTHON VARIABLES
+#
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+#
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(x)
+print(y)
+print(z)
+
+#
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+#
+a = 4
+A = "Sally"
+
+print(a)
+print(A)
+
+#
+x = "John"
+print(x)
+#double quotes are the same as single quotes:
+x = 'John'
+print(x)
